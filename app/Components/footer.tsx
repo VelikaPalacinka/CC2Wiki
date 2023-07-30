90571fe075e356745edb81a5a89c907596e93040
Hrvoje <hrvoje.orlic@pm.me>
2023-07-30 15:00:17 +0200

    new

--- app/Components/footer.tsx
+++ app/Components/footer.tsx
@@ -2,10 +2,10 @@ import style from './page.module.css';
 
 export default function footer() {
 	return (
-		<div className={style.footer}>
+		<footer className={style.footer}>
 			<p>Website Design © NonArte 2023.</p>
 			<p>Carrier Command 2 is © MicroProse and Developed by Geometa </p>
-			<p>Credit for Wiki content goes to Bedroll on discord</p>
-		</div>
+			<p>Credit for Wiki content goes to Bredroll on discord</p>
+		</footer>
 	);
 }
